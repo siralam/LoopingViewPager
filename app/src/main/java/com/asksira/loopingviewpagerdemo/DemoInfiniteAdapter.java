cc93ea3a2e31654db1d7b18157113cc7c0664bdd
Sira Lam <sira.lam@gravitas.com.hk>
2017-12-19 16:05:06 +0800

    Improved demo visual

--- app/src/main/java/com/asksira/loopingviewpagerdemo/DemoInfiniteAdapter.java
+++ app/src/main/java/com/asksira/loopingviewpagerdemo/DemoInfiniteAdapter.java
@@ -7,13 +7,12 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.asksira.loopingviewpager.LoopingPagerAdapter;
-import com.facebook.drawee.view.SimpleDraweeView;
 
 import java.util.ArrayList;
 
-public class DemoInfiniteAdapter extends LoopingPagerAdapter<PagerItem> {
+public class DemoInfiniteAdapter extends LoopingPagerAdapter<Integer> {
 
-    public DemoInfiniteAdapter(Context context, ArrayList<PagerItem> itemList, boolean isInfinite) {
+    public DemoInfiniteAdapter(Context context, ArrayList<Integer> itemList, boolean isInfinite) {
         super(context, itemList, isInfinite);
     }
 
@@ -23,10 +22,28 @@ public class DemoInfiniteAdapter extends LoopingPagerAdapter<PagerItem> {
             convertView = LayoutInflater.from(context).inflate(R.layout.item_pager, null);
             container.addView(convertView);
         }
-        SimpleDraweeView image = convertView.findViewById(R.id.image);
-        image.setImageURI(itemList.get(listPosition).getImageUrl());
+        convertView.findViewById(R.id.image).setBackgroundColor(context.getResources().getColor(getBackgroundColor(listPosition)));
         TextView description = convertView.findViewById(R.id.description);
-        description.setText(itemList.get(listPosition).getDescription());
+        description.setText(String.valueOf(itemList.get(listPosition)));
         return convertView;
+    }
+
+    private int getBackgroundColor (int number) {
+        switch (number) {
+            case 0:
+                return android.R.color.holo_red_light;
+            case 1:
+                return android.R.color.holo_orange_light;
+            case 2:
+                return android.R.color.holo_green_light;
+            case 3:
+                return android.R.color.holo_blue_light;
+            case 4:
+                return android.R.color.holo_purple;
+            case 5:
+                return android.R.color.black;
+            default:
+                return android.R.color.black;
+        }
     }
 }
--- app/src/main/java/com/asksira/loopingviewpagerdemo/MainActivity.java
+++ app/src/main/java/com/asksira/loopingviewpagerdemo/MainActivity.java
@@ -15,9 +15,9 @@ public class MainActivity extends AppCompatActivity {
     AutoScrollViewPager viewPager;
     DemoInfiniteAdapter adapter;
     PageIndicatorView indicatorView;
-    Button changeDatasetButton;
+    Button changeDataSetButton;
 
-    private int currentDataset = 1;
+    private int currentDataSet = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager = findViewById(R.id.viewpager);
         indicatorView = findViewById(R.id.indicator);
-        changeDatasetButton = findViewById(R.id.change_dataset);
-        changeDatasetButton.setOnClickListener(new View.OnClickListener() {
+        changeDataSetButton = findViewById(R.id.change_dataset);
+        changeDataSetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 changeDataset();
@@ -64,31 +64,31 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
-    private ArrayList<PagerItem> createDummyItems () {
-        ArrayList<PagerItem> items = new ArrayList<>();
-        items.add(0, new PagerItem("https://store.storeimages.cdn-apple.com/8750/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/default/macbook-default-image-201604?wid=2000&hei=856&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1493227953329", "Macbook"));
-        items.add(1, new PagerItem("https://cdn.shopify.com/s/files/1/1162/6052/products/product-image-mug_3_1024x1024.png?v=1458576844", "NesCafe"));
-        items.add(2, new PagerItem("https://d63z0236ucgyj.cloudfront.net/products/2805406/product/tempo-tissue-single-box-soft-strong-pack-of-80-574532cbb75a5.jpg", "Tempo"));
-        items.add(3, new PagerItem("https://i.ytimg.com/vi/yhjXTatF9dE/maxresdefault.jpg", "Sora no Kiseki"));
-        items.add(4, new PagerItem("https://www.daltonsbusiness.com/advice/wp-content/uploads/2017/10/mc.png", "McDonalds"));
-        items.add(5, new PagerItem("https://i.imgur.com/VtQ3BKV.jpg", "Sen no Kiseki"));
+    private ArrayList<Integer> createDummyItems () {
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(0, 1);
+        items.add(1, 2);
+        items.add(2, 3);
+        items.add(3, 4);
+        items.add(4, 5);
+        items.add(5, 6);
         return items;
     }
 
-    private ArrayList<PagerItem> createAnotherDummyItmes () {
-        ArrayList<PagerItem> items = new ArrayList<>();
-        items.add(0, new PagerItem("https://i.ytimg.com/vi/yhjXTatF9dE/maxresdefault.jpg", "Sora no Kiseki"));
-        items.add(1, new PagerItem("https://i.imgur.com/VtQ3BKV.jpg", "Sen no Kiseki"));
+    private ArrayList<Integer> createAnotherDummyItems() {
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(0, 1);
+        items.add(1, 2);
         return items;
     }
 
     private void changeDataset () {
-        if (currentDataset == 1) {
-            adapter.setItemList(createAnotherDummyItmes());
-            currentDataset = 2;
+        if (currentDataSet == 1) {
+            adapter.setItemList(createAnotherDummyItems());
+            currentDataSet = 2;
         } else {
             adapter.setItemList(createDummyItems());
-            currentDataset = 1;
+            currentDataSet = 1;
         }
         indicatorView.setCount(viewPager.getIndicatorCount());
     }
